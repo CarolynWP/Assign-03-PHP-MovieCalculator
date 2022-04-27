60c0049d6fa720e661eb452e94287fbe7bf0f546
Carolyn Webster-Pless <99274537+CarolynWP@users.noreply.github.com>
2022-04-27 00:03:48 +0000

    started it

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+		<!-- css -->
+		<link rel="stylesheet" href="./css/style.css" />
+		
+		<!-- title -->
+		<title>Movie calculator</title>
+	</head>
+	
+	<!-- body -->
+	<body>
